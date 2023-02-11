a216dc65babd521e6762ae5b0892ad0ae8c8cd83
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-11 23:35:39 +0200

    task 7-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,41 @@
+// const formEl = document.querySelector(".login-form");
+// const inputsEls = formEl.querySelectorAll("input");
+// const buttonEl = formEl.lastElementChild;
 
+// formEl.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+// if (inputsEls[0].value === "" && inputsEls[1].value === "") {
+//   return alert("Введіть данні");
+// } else if (inputsEls[0].value === "") {
+//   return alert("Введіть email");
+// } else if (inputsEls[1].value === "") {
+//   return alert("Введіть password");
+// }
+
+//   console.log(`Email: ${inputsEls[0].value}, Password: ${inputsEls[1].value}`);
+//   event.currentTarget.reset();
+// });
+
+const formEl = document.querySelector(".login-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" && password.value === "") {
+    return alert("Enter the data");
+  } else if (email.value === "") {
+    return alert("Enter the email");
+  } else if (password.value === "") {
+    return alert("Enter the password");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", handleSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const colorEl = document.querySelector(".color");
+const changeColorBtnEl = document.querySelector(".change-color");
+
+const changeColor = () => {
+  const color = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+};
+
+changeColorBtnEl.addEventListener("click", changeColor);
